601f98c2b8fb8ed30acf86547d684c79c15919b8
BugganeHuman <markizdelakarabas@gmail.com>
2026-01-17 19:41:34 +0500

    add auth

--- core/views.py
+++ core/views.py
@@ -1,17 +1,22 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 
 
 def signup(request):
-    if request.method == "post":
+    print("start signup")
+    if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
+            print("2------------")
             login(request, form.save())
+            print(form)
             return redirect("home")
     else:
         form = UserCreationForm()
     return render(request, "registration/signup.html", {"form": form})
 
+@login_required
 def home(request):
     return render(request, "base.html")
